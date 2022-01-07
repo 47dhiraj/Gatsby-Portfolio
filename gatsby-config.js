8ad27c75b10cddf1a9499ff70fc303ba27586945
dhirajkafle553 <dhirajkafle553@gmail.com>
2022-01-07 14:51:59 +0545

    Learned about gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,19 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
 
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+
+  ],
+
+  
   siteMetadata: {
 
     title: `Dhiraj Kafle`,
---
+++ src/projects/movie_recommendation_system.md
@@ -0,0 +1,7 @@
+---
+title: Movie Recommendation System
+stack: HTML, CSS, Bootstrap, Javascript & Django
+slug: movie-recommendation-system
+---
+
+Movie Recommendation system is developed using item-item based collaborative filtering with Pearson Coorelation Algorithm.
