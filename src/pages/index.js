49128bc947c8d3339a367ad0b1abc22f273a05ee
Dhiraj Kafle <dhirajkafle19@gmail.com>
2025-08-17 04:30:28 +0545

    updated .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,77 +1,92 @@
-import React, {useState, useEffect} from "react"
-import { graphql, Link } from 'gatsby'
-import Layout from "../components/Layout"
-import * as styles from '../styles/home.module.css'
-
-import { GitHub } from "@material-ui/icons"
-import { LinkedIn } from "@material-ui/icons"
-import { Facebook } from "@material-ui/icons"
-import Grid  from "@material-ui/core/Grid"
-import { IconButton } from "@material-ui/core"
-
-import Zoom from "@material-ui/core/Zoom"
-
-import { StaticImage } from "gatsby-plugin-image"
-
-
+import React, { useState, useEffect } from "react";
+import { graphql, Link } from 'gatsby';
+import Layout from "../components/Layout";
+import * as styles from '../styles/home.module.css';
+import { GitHub, LinkedIn, Facebook } from "@material-ui/icons";
+import { IconButton } from "@material-ui/core";
+import { StaticImage } from "gatsby-plugin-image";
 
 const socialItems = [
-  {icon: GitHub, url: "https://github.com/47dhiraj"},
-  {icon: LinkedIn, url: "https://www.linkedin.com/in/dhiraj-kafle-4a19781a3/"},
-  {icon: Facebook, url: "https://www.facebook.com/dhirajkafle553/"},
-]
-
+  { icon: GitHub, url: "https://github.com/47dhiraj" },
+  { icon: LinkedIn, url: "https://www.linkedin.com/in/dhiraj-kafle-4a19781a3/" },
+  { icon: Facebook, url: "https://www.facebook.com/dhirajkafle553/" }
+];
 
 export default function Home() {
+  const [displayed, setDisplayed] = useState("");
+  const [roleIndex, setRoleIndex] = useState(0);
 
+  const roles = ["Software Engineer", "Data Engineer"];
+  const typingSpeed = 100;
+  const holdDuration = 1000;
 
-  const [show, setShow] = useState(false)
-  useEffect(() => setShow(true), [])
+  useEffect(() => {
+    let charIndex = 0;
+    let typingInterval;
+
+    const typeRole = () => {
+      if (charIndex < roles[roleIndex].length) {
+        setDisplayed(prev => prev + roles[roleIndex][charIndex]);
+        charIndex++;
+      } else {
+        clearInterval(typingInterval);
+        setTimeout(() => {
+          setDisplayed("");
+          setRoleIndex((prev) => (prev + 1) % roles.length);
+        }, holdDuration);
+      }
+    };
+
+    typingInterval = setInterval(typeRole, typingSpeed);
+    return () => clearInterval(typingInterval);
+  }, [roleIndex]);
 
   return (
     <Layout>
-
       <section className={styles.header}>
         
-        <Zoom in={show} style={{ transitionDelay: show ? '200ms' : '0ms' }}>
-          <div>
-            <h2>Code, The Logic !</h2>
-            <h3>IT Engineer and Passionate Pragmatice Developer</h3>
+        {/* Left Column */}
+        <div className={styles.leftColumn}>
+          <h2>Code, The Logic !</h2>
+          <h3>IT Engineer and Passionate Pragmatic Developer</h3>
 
+          <h4 className={styles.typingFixed}>{displayed}</h4>
 
-            <a className={styles.btn} rel="noopener noreferrer" href={`./pdf/resume.pdf`} target="_blank"> View Resume </a>
-          
-          </div>
-        </Zoom>
-
-
-        <div style={{ display: 'flex', flexDirection: 'row'}}>
-
-          <Grid Container style={{ marginTop: 'auto', marginLeft: 'auto' }}>
-            {
-              socialItems.map((item) => (
-                <Grid item  key={item.icon}>
-                  <Link href={item.url} target='_blank'>
-                    <IconButton>
-                      <item.icon />
-                    </IconButton>
-                  </Link>
-                </Grid>
-              ))
-            }
-          </Grid>
-          
-          <StaticImage src="../images/profile.jpg" alt="Profile Pic" placeholder="blurred" layout="constrained" width={420} height={600} style={{borderRadius: '25px'}} />
-
+          <a className={styles.btn} 
+             rel="noopener noreferrer" 
+             href={`./pdf/resume.pdf`} 
+             target="_blank">
+            View Resume
+          </a>
         </div>
-        
+
+        {/* Right Column: Profile + Vertical Social Icons */}
+        <div className={styles.rightColumn}>
+          <div style={{ position: 'relative', display: 'inline-block' }}>
+            
+            <StaticImage 
+              src="../images/profile.jpg"
+              alt="Profile Pic"
+              placeholder="blurred"
+              layout="constrained"
+              width={320}
+              height={450}
+              style={{ borderRadius: '25px' }}
+            />
+
+            <div className={styles.socialVertical}>
+              {socialItems.map((item) => (
+                <Link href={item.url} target="_blank" key={item.icon}>
+                  <IconButton>
+                    <item.icon />
+                  </IconButton>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
 
       </section>
-
     </Layout>
-  )
-
+  );
 }
-
-
-
