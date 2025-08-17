8f78dab9d923fec60efe80f5f53f3b73e81494a7
Dhiraj Kafle <dhirajkafle19@gmail.com>
2025-08-17 21:57:56 +0545

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,8 +21,8 @@ export default function Home() {
   const [roleIndex, setRoleIndex] = useState(0);
 
   const roles = ["Software Engineer", "Data Engineer"];
-  const typingSpeed = 220;
-  const holdDuration = 700;
+  const typingSpeed = 170;
+  const holdDuration = 500;
 
 
   useEffect(() => {
