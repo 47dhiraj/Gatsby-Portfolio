1bf23f855e9a6af31c72c4dfbbfc46401e45bb7a
Dhiraj Kafle <dhirajkafle19@gmail.com>
2025-09-23 19:26:56 +0545

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -64,7 +64,7 @@ export default function Home() {
           <h2> Code, The Logic ! </h2>
           <h3> 'Transform Data into AI Insights' </h3>
 
-          <div style={{ height: '2em' }}>
+          <div>
             <h4 className={styles.typingFixed}>A {displayed}</h4>
           </div>
 
