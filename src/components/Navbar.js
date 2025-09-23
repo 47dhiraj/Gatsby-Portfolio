247cc9e213d5fe225a510896702e7d3eb1223087
Dhiraj Kafle <dhirajkafle19@gmail.com>
2025-09-24 00:31:27 +0545

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,61 +1,99 @@
 import React from 'react'
 import { graphql, Link, useStaticQuery } from 'gatsby'
 import { StaticImage } from "gatsby-plugin-image"
-
 import HomeIcon from "@material-ui/icons/Home";
+
 
 
 export default function Navbar() {
 
-    const data = useStaticQuery(graphql`                   
-        {
-            site {
-                siteMetadata {
-                title
-                }
-            }
+
+    const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
         }
-    `)
+      }
+    }
+  `)
+
 
     const { title } = data.site.siteMetadata
 
 
     return (
+
         <nav className='header'>
 
 
-            <div class="navbar__brand">
-
-                <StaticImage src="../images/avatar.png" alt="A dinosaur" placeholder="blurred" layout="constrained" width={35} />
-                {/* <img class="navbar__logo" src="/avatar.png" alt="FullStack Web Developer, Nepal" /> */}
+            <div className="navbar__brand">
+                <StaticImage
+                    src="../images/avatar.png"
+                    alt="A dinosaur"
+                    placeholder="blurred"
+                    layout="constrained"
+                    width={35}
+                />
 
                 <Link className="nav-title-link" to="/">
-                    <strong style={{ fontSize: '1.1em', maxWidth: '100%' }}>{ title }</strong>
+                    <strong style={{ fontSize: '1.1em', maxWidth: '100%' }}>{title}</strong>
                 </Link>
-
             </div>
-
 
 
             <div className="links">
 
-                <Link to="/" className="nav-link">
-
+                <Link
+                    to="/"
+                    getProps={({ isCurrent }) =>
+                        isCurrent ? { className: "active-link", onClick: e => e.preventDefault() } : {}
+                    }
+                >
                     <HomeIcon className="nav-icon" />
-
-                    <span className="nav-text"> Home </span>
-
+                    <span className="nav-text">Home</span>
                 </Link>
 
-                <Link to="/about"> About Me </Link>
-                <Link to="/skills"> Skills </Link>
-                <Link to="/projects"> Projects </Link>
-                <Link to="/blogs"> Blogs </Link>
+                <Link
+                    to="/about"
+                    getProps={({ isCurrent }) =>
+                        isCurrent ? { className: "active-link", onClick: e => e.preventDefault() } : {}
+                    }
+                >
+                    About Me
+                </Link>
 
-                {/* <a rel="noopener noreferrer" href={`./pdf/resume.pdf`} target="_blank"> View Resume </a> */}
+                <Link
+                    to="/skills"
+                    getProps={({ isCurrent }) =>
+                        isCurrent ? { className: "active-link", onClick: e => e.preventDefault() } : {}
+                    }
+                >
+                    Skills
+                </Link>
 
+                <Link
+                    to="/projects"
+                    getProps={({ isCurrent }) =>
+                        isCurrent ? { className: "active-link", onClick: e => e.preventDefault() } : {}
+                    }
+                >
+                    Projects
+                </Link>
+
+                <Link
+                    to="/blogs"
+                    getProps={({ isCurrent }) =>
+                        isCurrent ? { className: "active-link", onClick: e => e.preventDefault() } : {}
+                    }
+                >
+                    Blogs
+                </Link>
+                
             </div>
 
         </nav>
+
     )
+
 }
